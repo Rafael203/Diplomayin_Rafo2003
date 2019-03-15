25fc190c43d9ec26cae35302e2ed1c9ba87406bd
Rafael203 <khrafael2003@gmail.com>
2019-03-15 19:58:59 +0400

    layout half connect

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,9 +12,23 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/style.css',
+        'css/bootstrap.css',
+        'css/font-awesome.min.css',
+        'css/flexslider.css',
+
     ];
     public $js = [
+        'js/minicart.js',
+        'js/jquery-1.11.1.min.js',
+        'js/jquerywmuSlider.js',
+        'js/jqueryflexslider.js',
+        'js/jqueryflexisel.js',
+        'js/imagezoom.js',
+        'js/easyResponsiveTabs.js',
+        'js/bootstrap.js'
+
+
     ];
     public $depends = [
         'yii\web\YiiAsset',
